7df0891e54c146cbac5d9e00ebb7e2098fbb994d
VictorSheep <victor.moutton@gmail.com>
2017-12-01 16:56:16 +0400

    add utils file : for server

---
+++ server/src/utils.js
@@ -0,0 +1,9 @@
+module.exports = {
+	foundVerify : ( item, res, errMessage ) => {
+		if( item === null || !item ) {
+			res.json( {status: 404, error: 1, message: errMessage} )
+		}	else {
+			return item
+		}
+	}
+}
