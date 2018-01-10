8f7e3bddc1a0028735380bec0aa171be65a7551d
VictorSheep <victor.moutton@gmail.com>
2018-01-10 22:48:22 +0400

    update : design admin login for mvp

--- client/admin/src/App.vue
+++ client/admin/src/App.vue
@@ -18,10 +18,18 @@ export default {
 
 body {
 	margin: 0;
+	background-color: #FCFCFC; 
 }
 
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   color: #2c3e50;
 }
+
+@media (max-width: 640px) {
+  body {
+  	font-size: 13px;
+  }
+}
+
 </style>
