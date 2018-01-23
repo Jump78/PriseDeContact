a628493392907719194287a459bd67867cef83e6
Clement Teboul <teboul.clement@gmail.com>
2018-01-23 09:08:23 +0100

    refacto apiService: Add comment

--- client/prospect/src/services/apiService.js
+++ client/prospect/src/services/apiService.js
@@ -1,20 +1,25 @@
 import config from '../../config/config';
 
+/**
+ * Base class of service
+ * @type class
+ */
 export default class {
 
   constructor () {
-    this.baseUrl= config.apiEndPoint+':'+config.apiPort;
+    this.baseUrl= config.apiEndPoint;
   }
 
+  //Check for http errors
   async checkErrors(res) {
     if(res.ok) return res;
 
-    let errorMsg = `ERROR ${res.status} (${res.statusText})`;
-    let serverText = await res.text();
-    if(serverText) errorMsg = `${errorMsg}: ${serverText}`;
+    let errorMsg = `ERROR ${res.status} (${res.statusText})`; //Create the error message
+    let serverText = await res.text(); //Wait for the response's text
+    if(serverText) errorMsg = `${errorMsg}: ${serverText}`; //If we have texte, add it to the error message
 
-    var error = new Error(errorMsg);
-    error.response = res;
+    var error = new Error(errorMsg); //Create the error
+    error.response = res; //Set the response into the error
     throw error;
   }
 }
