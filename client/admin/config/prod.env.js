76748c2bc4fbb488b3678562710516df61d6185c
Clement Teboul <teboul.clement@gmail.com>
2018-03-15 14:23:19 +0100

    update structure: Use prod.env and dev.env to know what socket io config we have to use

--- client/admin/config/prod.env.js
+++ client/admin/config/prod.env.js
@@ -1,4 +1,6 @@
 'use strict'
 module.exports = {
-  NODE_ENV: '"production"'
+  NODE_ENV: '"production"',
+  apiEndPoint: "https://empdc.alwaysdata.net/api",
+  formUrl: "https://empdc.alwaysdata.net"
 }
